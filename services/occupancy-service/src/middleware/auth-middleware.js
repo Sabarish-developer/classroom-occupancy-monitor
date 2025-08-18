6450567a70560b60617c4fd2feb2faeea9cd6192
Sabarish-developer <sabarish.cspro@gmail.com>
2025-08-18 12:26:46 +0530

    feat: add auth middleware to check jwt

--- services/occupancy-service/src/middleware/auth-middleware.js
+++ services/occupancy-service/src/middleware/auth-middleware.js
@@ -1,23 +1,24 @@
 import { verifyJwt } from "../utils/jwt.js";
 
-const authMiddleware = (req, res, next) => {
-    try{
-        const token = req.cookies.token;
-        if(!token) return res.status(401).json({error: "No token found"});
+const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) return res.status(401).json({ error: "No token found" });
 
-        const {valid, decoded, error} = verifyJwt(token);
-        if(valid){
-            req.user = decoded;
-            next();
-        }
-        else throw new Error(error || "Token invalid");
+    const { valid, payload, error } = await verifyJwt(token);
+    if (valid) {
+      req.user = payload; // attach decoded payload to request
+      next();
+    } else {
+      throw new Error(error || "Token invalid");
     }
-    catch(e){
-        if(e.name === "TokenExpiredError")
-            return res.status(401).json({error: "Token expired. Please login again"});
-        else
-            return res.status(401).json({error: "Token invalid"});
+  } catch (e) {
+    if (e.name === "TokenExpiredError") {
+      return res.status(401).json({ error: "Token expired. Please login again" });
+    } else {
+      return res.status(401).json({ error: "Token invalid" });
     }
-}
+  }
+};
 
 export default authMiddleware;
