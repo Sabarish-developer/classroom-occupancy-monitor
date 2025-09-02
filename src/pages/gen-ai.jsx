687e78f8ef91c42e190de6a540a0b287d152468a
Sabarish-developer <sabarish.cspro@gmail.com>
2025-09-02 17:26:08 +0530

    refact: adjust sidebar in genai page

--- src/pages/gen-ai.jsx
+++ src/pages/gen-ai.jsx
@@ -1,11 +1,15 @@
 import Sidebar from "@/components/sidebar";
 import ChatInterface from "@/components/ai-chat";
+import { useState } from "react";
 
 export const GenAi = () => {
+
+    const [open, setOpen] = useState(false); //sidebar state
+
     return (
         <div className="flex h-screen overflow-hidden">
-            <Sidebar />
-            <div className="flex-1 h-full ">
+            <Sidebar open={open} setOpen={setOpen}/>
+            <div className={`flex-1 ${open ? "md:ml-[16rem]" : "md:ml-[4rem]"} transition-all duration-500`}>
                 <ChatInterface />
             </div>
         </div>
